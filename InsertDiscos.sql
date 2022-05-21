3bb545a92d06c8d27f0036efdd2a3ff80f2ba40b
Guilherme Augusto da Silva <44906357+GASJW@users.noreply.github.com>
2022-05-21 15:49:13 -0300

    Listagem de Discos com datetype

---
+++ InsertDiscos.sql
@@ -0,0 +1,12 @@
+USE [Bib]
+GO
+
+INSERT INTO [dbo].[Disco] ([NomeDisco], [NomeAutor], [NumeroDeMusicas], [DataLancamento])
+     VALUES('OK ORCHESTRA', 'AJR', 10, '2021-03-26')
+INSERT INTO [dbo].[Disco] ([NomeDisco], [NomeAutor], [NumeroDeMusicas], [DataLancamento])
+     VALUES('Born to Die', 'Lana Del Rey', 15, '2012-01-27')
+INSERT INTO [dbo].[Disco] ([NomeDisco], [NomeAutor], [NumeroDeMusicas], [DataLancamento])
+     VALUES('AM', 'Arctic Monkeys', 14, '2013-09-09')
+INSERT INTO [dbo].[Disco] ([NomeDisco], [NomeAutor], [NumeroDeMusicas], [DataLancamento])
+     VALUES('Facelift', 'Alice in Chains', 10, '1992-09-19')
+GO
